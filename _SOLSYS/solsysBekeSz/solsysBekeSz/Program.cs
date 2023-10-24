67b7922fccce49777546b94467ae39fd95e7eb3e
bekeszabi26 <145983566+bekeszabi26@users.noreply.github.com>
2023-10-24 13:37:42 +0200

    Add files via upload

---
+++ _SOLSYS/solsysBekeSz/solsysBekeSz/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace solsysBekeSz
+{
+    internal class Program
+    {
+        static double atlagB(List<solsys> s)
+        {
+            return s.Average(a => a.holdszam);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. feladat:");
+            var bolygok = new List<solsys>();
+            foreach (var i in File.ReadAllLines(@"..\..\src\solsys.txt"))
+            {
+                bolygok.Add(new solsys(i));
+            }
+            Console.WriteLine($"\t3.1: {bolygok.Count} bolgó van a naprendszerben");
+
+            Console.WriteLine($"\t3.2: a naprandszerben a bolygóknak átlagosan {atlagB(bolygok)} holdja van");
+
+            var f3 = bolygok.OrderBy(a => a.bterfogat).Last();
+            Console.WriteLine($"\t3.3: a legnagyobb térfogatú bolgó: {f3.bneve}");
+
+            Console.Write($"\t3.4: írd be a keresett bolygó nevét: ");
+            string beirt = Console.ReadLine();
+            var f4 = bolygok.SingleOrDefault(x => x.bneve == beirt);
+            Console.WriteLine(f4 != null ? "\tVan ilyen bolygó a naprendszerben." : "\tsajnos nincs ilyen nevű bolygó a naprendszerben");
+           
+            Console.Write("\t3.5 írj be egy egész számot: ");
+            string beirt2 = Console.ReadLine();
+            var f5 = bolygok.Where(x => x.holdszam > int.Parse(beirt2));
+            Console.WriteLine($"\ta következő bolygóknak van {beirt2}-nél több holdja: ");
+            foreach (var i in f5)
+            {
+                Console.Write($"\t[{i.bneve}],");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ _SOLSYS/solsysBekeSz/solsysBekeSz/solsys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solsysBekeSz
+{
+    class solsys
+    {
+        public string bneve { get; set; }
+        public int holdszam { get; set; }
+        public double bterfogat { get; set; }
+        public solsys(string s) 
+        {
+        string[] v = s.Split(';');
+            bneve = v[0];
+            holdszam = int.Parse(v[1]);
+            bterfogat = Double.Parse(v[2]);
+        }
+    }
+}
